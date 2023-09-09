fd217c9bf7c5978b18e9cbe4554874692a270625
Andy Ma <solarcactus@live.com>
2023-09-09 00:55:07 -0700

    dvd converted to 3d cube

--- GLTutorial/frag.shader
+++ GLTutorial/frag.shader
@@ -1,14 +1,11 @@
 #version 330 core
 
 out vec4 FragColor;
-in vec3 ourColor;
 in vec2 TexCoord;
 
 uniform sampler2D texture1;
-uniform sampler2D texture2;
-uniform vec4 currColor;
 
 void main()
 {
-    FragColor = currColor * texture(texture1, TexCoord) * texture(texture2, TexCoord);
+    FragColor = texture(texture1, TexCoord);
 }
--- GLTutorial/main.cpp
+++ GLTutorial/main.cpp
@@ -30,28 +30,67 @@ int main(void)
     Shader shaderProgramObj = Shader::Shader("vertex.shader", "frag.shader");
     unsigned int shaderProgram = shaderProgramObj.createShaderProgram();
     shaderProgramObj.addUniform1i("texture1", 0);
-    shaderProgramObj.addUniform1i("texture2", 1);
 
+
+    /*
     float vertices[] = {    
              // pos               color         tex-coords
         -0.25f, 0.25f, 0.0f,  1.0f, 1.0f, 0.0f,   0.0f, 1.0f, // top-left (0)
         0.25f, 0.25f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,  // top-right (1)
         -0.25f, -0.25f, 0.0f, 0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom-left (2)
         0.25f, -0.25f, 0.0f,  0.0f, 1.0f, 0.0f,   1.0f, 0.0f  // bottom-right (3)
+    };  
+    */
+    float vertices[] = {
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
 
-    unsigned int indices[] = {
-        0, 1, 2,
-        2, 3, 1
-    };
 
     // Intialize vertex array and vertex buffer object
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
     unsigned int VBO;
     glGenBuffers(1, &VBO);
-    unsigned int EBO;
-    glGenBuffers(1, &EBO);
 
     TextureLoader loader;
     Texture texture1 = Texture::Texture(0, "dvd.png");
@@ -59,84 +98,45 @@ int main(void)
     loader.addTexture(0, texture1);
     loader.loadTexture(0);
 
-    Texture texture2 = Texture::Texture(1, "container.jpg");
-
-    loader.addTexture(1, texture2);
-    loader.loadTexture(1);
-    
-
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // position
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)  0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    // color
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    // uv
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
-
-    // tex-coord
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-    glEnableVertexAttribArray(2);
-
-    float x = 0.0f;
-    float y = 0.25f;
-    float dx = 0.01f;
-    float dy = 0.01f;
-
-    glm::mat4 trans = glm::mat4(1.0f);
-
-    float colors[4][3] = { {1.0f, 1.0f, 1.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 1.0f, 0.0f}, {0.0f, 0.0f, 1.0f} };
-    float r = 1.0f;
-    float g = 1.0f;
-    float b = 1.0f;
-
-    int i = 0;
 
     /* Loop until the user closes the window */ 
     while (!glfwWindowShouldClose(window))
     {   
         /* Input check */   
-
+            
         /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        glEnable(GL_DEPTH_TEST);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
         loader.bindTextures();
 
-        float time = glfwGetTime();
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
 
-        if (x >= 1.0f || x <= -1.0f) {
-            dx = -dx;
-            if (i < 3) {
-                i += 1;
-            }
-            else {
-                i = 0;
-            }
-            
+        glm::mat4 view = glm::mat4(1.0f);
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 
-            g = colors[i][1];
-            b = colors[i][2];
-        }
-        if (y >= 1.0f || y <= -1.0f) {
-            dy = -dy;
-        }
-        
-        x += dx;
-        y += dy;
-            
-        trans = glm::translate(trans, glm::vec3(dx, dy, 0.0f));
+        glm::mat4 projection;
+        projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
-        shaderProgramObj.addUniformMat4("trans", trans);
-        shaderProgramObj.addUniform4f("currColor", 1.0f - sin(x) / 2, g, b, 1.0f);
+        shaderProgramObj.addUniformMat4("model", model);
+        shaderProgramObj.addUniformMat4("view", view);
+        shaderProgramObj.addUniformMat4("projection", projection);
         glUseProgram(shaderProgram);
 
         glBindVertexArray(VAO); 
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
